767ab43002c87454f117aa9819aac8b3602e366e
Pozitronik <antikillerxxl@gmail.com>
2025-11-24 18:11:53 +0400

    Implementing internal fonts & glyphs support

---
+++ internal/bitmap/glyphs/font_3x5.go
@@ -0,0 +1,560 @@
+package glyphs
+
+// Font3x5 is a compact 3×5 pixel monospace font
+// Extracted and expanded from internal/widget/doom.go
+// Best for small displays where space is limited
+var Font3x5 = &GlyphSet{
+	Name:        "3x5",
+	GlyphWidth:  3,
+	GlyphHeight: 5,
+	Glyphs: map[rune]*Glyph{
+		// Uppercase letters A-Z
+		'A': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'B': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{true, false, true},
+				{true, true, false},
+				{true, false, true},
+				{true, true, false},
+			},
+		},
+		'C': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{true, false, false},
+				{true, false, false},
+				{false, true, true},
+			},
+		},
+		'D': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, true, false},
+			},
+		},
+		'E': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, false},
+				{true, true, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+		'F': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, false},
+				{true, true, false},
+				{true, false, false},
+				{true, false, false},
+			},
+		},
+		'G': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{true, false, true},
+				{true, false, true},
+				{false, true, true},
+			},
+		},
+		'H': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'I': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+				{true, true, true},
+			},
+		},
+		'J': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, true},
+				{false, false, true},
+				{false, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'K': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, true, false},
+				{true, false, false},
+				{true, true, false},
+				{true, false, true},
+			},
+		},
+		'L': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, false},
+				{true, false, false},
+				{true, false, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+		'M': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, true, true},
+				{true, true, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'N': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, true, true},
+				{true, true, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'O': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'P': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{true, false, true},
+				{true, true, false},
+				{true, false, false},
+				{true, false, false},
+			},
+		},
+		'Q': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, true, true},
+				{false, false, true},
+			},
+		},
+		'R': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{true, false, true},
+				{true, true, false},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'S': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{false, true, false},
+				{false, false, true},
+				{true, true, false},
+			},
+		},
+		'T': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+			},
+		},
+		'U': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'V': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'W': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+			},
+		},
+		'X': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'Y': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+			},
+		},
+		'Z': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{false, false, true},
+				{false, true, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+
+		// Lowercase letters a-z
+		'a': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'd': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, true},
+				{false, false, true},
+				{false, true, true},
+				{true, false, true},
+				{false, true, true},
+			},
+		},
+		'e': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, false},
+				{true, true, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+		'f': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{true, true, false},
+				{true, false, false},
+				{true, false, false},
+			},
+		},
+		'g': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{true, false, true},
+				{true, false, true},
+				{false, true, true},
+			},
+		},
+		'i': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{false, false, false},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+			},
+		},
+		'l': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, false},
+				{true, false, false},
+				{true, false, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+		'n': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+			},
+		},
+		'o': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'w': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+			},
+		},
+
+		// Digits 0-9
+		'0': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, false, true},
+				{true, false, true},
+				{true, false, true},
+				{false, true, false},
+			},
+		},
+		'1': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{true, true, false},
+				{false, true, false},
+				{false, true, false},
+				{true, true, true},
+			},
+		},
+		'2': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, false},
+				{false, false, true},
+				{false, true, false},
+				{true, false, false},
+				{true, true, true},
+			},
+		},
+		'3': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{false, false, true},
+				{false, true, true},
+				{false, false, true},
+				{true, true, true},
+			},
+		},
+		'4': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{true, false, true},
+				{true, true, true},
+				{false, false, true},
+				{false, false, true},
+			},
+		},
+		'5': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, false},
+				{true, true, true},
+				{false, false, true},
+				{true, true, true},
+			},
+		},
+		'6': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, true},
+				{true, false, false},
+				{true, true, true},
+				{true, false, true},
+				{true, true, true},
+			},
+		},
+		'7': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{false, false, true},
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+			},
+		},
+		'8': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, true},
+				{true, true, true},
+				{true, false, true},
+				{true, true, true},
+			},
+		},
+		'9': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, true, true},
+				{true, false, true},
+				{true, true, true},
+				{false, false, true},
+				{true, true, false},
+			},
+		},
+
+		// Basic punctuation and symbols
+		' ': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+			},
+		},
+		'!': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, true, false},
+				{false, true, false},
+				{false, true, false},
+				{false, false, false},
+				{false, true, false},
+			},
+		},
+		'%': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{true, false, true},
+				{false, false, true},
+				{false, true, false},
+				{true, false, false},
+				{true, false, true},
+			},
+		},
+		'.': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, true, false},
+			},
+		},
+		',': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, true, false},
+				{true, false, false},
+			},
+		},
+		':': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, false},
+				{false, true, false},
+				{false, false, false},
+				{false, true, false},
+				{false, false, false},
+			},
+		},
+		'-': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, false},
+				{false, false, false},
+				{true, true, true},
+				{false, false, false},
+				{false, false, false},
+			},
+		},
+		'/': {
+			Width: 3, Height: 5,
+			Data: [][]bool{
+				{false, false, true},
+				{false, false, true},
+				{false, true, false},
+				{true, false, false},
+				{true, false, false},
+			},
+		},
+	},
+	Icons: nil,
+}
--- internal/widget/doom.go
+++ internal/widget/doom.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/AndreRenaud/gore"
+	"github.com/pozitronik/steelclock-go/internal/bitmap/glyphs"
 	"github.com/pozitronik/steelclock-go/internal/config"
 )
 
@@ -220,7 +221,10 @@ func (w *DoomWidget) Render() (image.Image, error) {
 
 	// Show download error if any
 	if w.downloadError != nil {
-		w.drawText(img, "Download failed!", pos.W/2, pos.H/2-4)
+		text := "Download failed!"
+		textWidth := glyphs.MeasureText(text, glyphs.Font3x5)
+		textX := (pos.W - textWidth) / 2
+		glyphs.DrawText(img, text, textX, pos.H/2-4, glyphs.Font3x5, color.Gray{Y: 255})
 		return img, nil
 	}
 
@@ -241,8 +245,11 @@ func (w *DoomWidget) Render() (image.Image, error) {
 
 // drawProgressBar renders a download progress bar
 func (w *DoomWidget) drawProgressBar(img *image.Gray, progress float64, width, height int) {
-	// Draw title
-	w.drawText(img, "Downloading DOOM", width/2, 4)
+	// Draw title using 3×5 pixel font
+	title := "Downloading DOOM"
+	titleWidth := glyphs.MeasureText(title, glyphs.Font3x5)
+	titleX := (width - titleWidth) / 2
+	glyphs.DrawText(img, title, titleX, 4, glyphs.Font3x5, color.Gray{Y: 255})
 
 	// Progress bar dimensions
 	barWidth := width - 20
@@ -268,67 +275,11 @@ func (w *DoomWidget) drawProgressBar(img *image.Gray, progress float64, width, h
 		}
 	}
 
-	// Draw percentage text
+	// Draw percentage text using 3×5 pixel font
 	percentText := fmt.Sprintf("%.0f%%", progress*100)
-	w.drawText(img, percentText, width/2, barY+barHeight+6)
-}
-
-// drawText draws centered text on the image (simple 3x5 pixel font)
-func (w *DoomWidget) drawText(img *image.Gray, text string, centerX, y int) {
-	charWidth := 4
-	totalWidth := len(text) * charWidth
-	x := centerX - totalWidth/2
-
-	for i, ch := range text {
-		w.drawChar(img, ch, x+i*charWidth, y)
-	}
-}
-
-// drawChar draws a single character using 3x5 pixel patterns
-func (w *DoomWidget) drawChar(img *image.Gray, ch rune, x, y int) {
-	white := color.Gray{Y: 255}
-
-	patterns := map[rune][][]bool{
-		'D': {{true, true, false}, {true, false, true}, {true, false, true}, {true, false, true}, {true, true, false}},
-		'o': {{false, true, false}, {true, false, true}, {true, false, true}, {true, false, true}, {false, true, false}},
-		'w': {{true, false, true}, {true, false, true}, {true, false, true}, {true, true, true}, {true, false, true}},
-		'n': {{true, true, false}, {true, false, true}, {true, false, true}, {true, false, true}, {true, false, true}},
-		'l': {{true, false, false}, {true, false, false}, {true, false, false}, {true, false, false}, {true, true, true}},
-		'a': {{false, true, false}, {true, false, true}, {true, true, true}, {true, false, true}, {true, false, true}},
-		'd': {{false, false, true}, {false, false, true}, {false, true, true}, {true, false, true}, {false, true, true}},
-		'i': {{false, true, false}, {false, false, false}, {false, true, false}, {false, true, false}, {false, true, false}},
-		'g': {{false, true, true}, {true, false, false}, {true, false, true}, {true, false, true}, {false, true, true}},
-		'M': {{true, false, true}, {true, true, true}, {true, true, true}, {true, false, true}, {true, false, true}},
-		'O': {{false, true, false}, {true, false, true}, {true, false, true}, {true, false, true}, {false, true, false}},
-		' ': {{false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}},
-		'%': {{true, false, true}, {false, false, true}, {false, true, false}, {true, false, false}, {true, false, true}},
-		'0': {{false, true, false}, {true, false, true}, {true, false, true}, {true, false, true}, {false, true, false}},
-		'1': {{false, true, false}, {true, true, false}, {false, true, false}, {false, true, false}, {true, true, true}},
-		'2': {{true, true, false}, {false, false, true}, {false, true, false}, {true, false, false}, {true, true, true}},
-		'3': {{true, true, true}, {false, false, true}, {false, true, true}, {false, false, true}, {true, true, true}},
-		'4': {{true, false, true}, {true, false, true}, {true, true, true}, {false, false, true}, {false, false, true}},
-		'5': {{true, true, true}, {true, false, false}, {true, true, true}, {false, false, true}, {true, true, true}},
-		'6': {{false, true, true}, {true, false, false}, {true, true, true}, {true, false, true}, {true, true, true}},
-		'7': {{true, true, true}, {false, false, true}, {false, true, false}, {false, true, false}, {false, true, false}},
-		'8': {{true, true, true}, {true, false, true}, {true, true, true}, {true, false, true}, {true, true, true}},
-		'9': {{true, true, true}, {true, false, true}, {true, true, true}, {false, false, true}, {true, true, false}},
-		'f': {{false, true, true}, {true, false, false}, {true, true, false}, {true, false, false}, {true, false, false}},
-		'e': {{true, true, true}, {true, false, false}, {true, true, false}, {true, false, false}, {true, true, true}},
-		'!': {{false, true, false}, {false, true, false}, {false, true, false}, {false, false, false}, {false, true, false}},
-	}
-
-	pattern, ok := patterns[ch]
-	if !ok {
-		return
-	}
-
-	for row := 0; row < len(pattern) && row < 5; row++ {
-		for col := 0; col < len(pattern[row]) && col < 3; col++ {
-			if pattern[row][col] {
-				img.SetGray(x+col, y+row, white)
-			}
-		}
-	}
+	textWidth := glyphs.MeasureText(percentText, glyphs.Font3x5)
+	textX := (width - textWidth) / 2
+	glyphs.DrawText(img, percentText, textX, barY+barHeight+6, glyphs.Font3x5, color.Gray{Y: 255})
 }
 
 // Update is called periodically
